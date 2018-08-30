d1d6a017f270175d90819e581fe11a6494ce862e
Patricio Torello <patriciotorello@gmail.com>
2018-08-30 17:50:59 -0300

    Final sin details

--- acamica-recursos-home-banking/js/homebanking.js
+++ acamica-recursos-home-banking/js/homebanking.js
@@ -4,8 +4,20 @@ var nombreUsuario = ("Patricio Torello");
 var saldoCuenta = 3800;
 var limiteExtraccion = 1000;
 
+var agua = 350;
+var telefono = 425;
+var luz = 210 ;
+var internet = 570 ;
+
+var cuentaAmiga1 = 1234567 ;
+var cuentaAmiga2 = 7654321 ;
+
+var codigoSeguridad = 2708;
+
+
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
+	iniciarSesion();
     cargarNombreEnPantalla();
     actualizarSaldoEnPantalla();
     actualizarLimiteEnPantalla();
@@ -25,30 +37,109 @@ function restarDinero (dinero){
 }
 
 function cambiarLimiteDeExtraccion() {
+	var nuevoLimite = parseInt(prompt("Ingrese nuevo limite de extraccion"));
+	limiteExtraccion = nuevoLimite;
+	actualizarLimiteEnPantalla();
+	alert ("Nuevo limite de extracción: " + nuevoLimite);
+}
 
+function haySaldoDisponible (dinero) {
+	var dineroInicial = saldoCuenta;
+	if (dinero < limiteExtraccion && dinero < saldoCuenta && dinero % 100 == 0){
+		restarDinero (dinero);
+		actualizarSaldoEnPantalla();
+		alert ("Saldo anterior: " + dineroInicial + "\nDinero extraido: " + dinero + "\nSaldo actual: " + saldoCuenta );		
+	} else if (dinero > saldoCuenta) {
+		alert ("No hay saldo disponible en tu cuenta para extraer esa cantidad de dinero.");
+	}  else if (dinero >= limiteExtraccion) {
+		alert ("La cantidad de dinero que deseas extraer, es mayor a tu limite de extracción");
+	}  else if (dinero % 100 != 0) {
+		alert ("Solo puedes extraer billetes de $100.");
+	};  
 }
 
 function extraerDinero() {
-
+	var dineroExtraido = parseInt(prompt("Cuanto dinero desea extraer?"));
+	haySaldoDisponible (dineroExtraido);
 }
+
 
 function depositarDinero() {
+	var dineroInicial = saldoCuenta;
 	var dineroDepositado = parseInt(prompt("Cuanto dinero desea depositar?"));
 	sumarDinero (dineroDepositado);
-	return actualizarSaldoEnPantalla(); 
+	actualizarSaldoEnPantalla();
+	alert ("Saldo anterior: " + dineroInicial + "\nDinero depositado: " + dineroDepositado + "\nSaldo actual: " + saldoCuenta );
 }
+
+
+
+function servicioPagar (servicio, elnombredelservicio) {
+	var saldoCuentaAntesDePagarServicios = saldoCuenta;
+	if (servicio < saldoCuenta) {
+			restarDinero(servicio);
+			actualizarSaldoEnPantalla();
+			alert ("Has pagado el servicio de " + elnombredelservicio + "\n 1- Saldo Anterior: " + saldoCuentaAntesDePagarServicios + "\n 2- Dinero descontado: " + servicio + "\n 3- Saldo actual: " + saldoCuenta );
+	} else {
+			alert ("No hay suficiente saldo en tu cuenta para pagar este servicio.");
+	};
+}
+
 
 function pagarServicio() {
-
+	var servicio = window.prompt ("Ingrese el numero que corresponda con el numero que queres pagar: \n1- Agua \n2- Luz \n3- Internet \n4- Telefono" );
+	switch ( servicio ) {
+		case "1":
+			servicioPagar (agua, "agua");
+			break;
+		case "2":
+			servicioPagar (luz, "luz");
+			break;
+		case "3":
+			servicioPagar (internet, "internet");
+			break;
+		case "4":
+			servicioPagar (telefono, "telefono");
+			break;
+		};
 }
+
+
 
 function transferirDinero() {
-
+	var dineroInicial = saldoCuenta;
+	var dineroTransferido = parseInt(prompt("Cuanto dinero desea transferir?"));
+	if (dineroTransferido <= dineroInicial) {
+		var cuentaDestino = parseInt(prompt("A que nro de cuenta desea transferir?"));
+		if (cuentaDestino == cuentaAmiga1 || cuentaDestino == cuentaAmiga2) {
+			restarDinero(dineroTransferido);
+			actualizarSaldoEnPantalla();
+			alert ("Se han transferido " + dineroTransferido + "\nCuenta destino: " + cuentaDestino );
+		} else {
+			alert ("Solo puede transferirse dinero a una cuenta amiga.");
+		}
+	} else {
+		alert("No puede transferirse esa cantidad de dinero.");
+	}
+	
 }
+
+
 
 function iniciarSesion() {
-
+	var inicioSesion = parseInt(prompt("Ingrese el codigo de seguridad de su cuenta"));
+	if (inicioSesion == codigoSeguridad) {
+		alert("Bienvenido Patricio Torello, ya puedes comenzar a hacer operaciones.");
+	} else {
+		var bloquearSaldo = 0;
+		saldoCuenta = bloquearSaldo;
+		actualizarSaldoEnPantalla();
+		alert("Tu dinero ha sido retenido por razones de seguridad");
+	}
 }
+
+
+
 
 //Funciones que actualizan el valor de las variables en el HTML
 function cargarNombreEnPantalla() {
